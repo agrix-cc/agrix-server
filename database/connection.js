fd9ed57dbe369724f0eaea56677cf301edf84d3b
dilankayasuru <dilankayasuru001@gmail.com>
2025-01-08 09:51:26 +0530

    add ssl certificate

--- database/connection.js
+++ database/connection.js
@@ -1,17 +1,29 @@
-const {Sequelize} = require('sequelize');
+const { Sequelize } = require('sequelize');
+const fs = require('fs');
+const path = require('path');
+
 // Database credentials from .env
 const DB_HOST = process.env.DB_HOST;
 const DB_PORT = process.env.DB_PORT;
 const DB_NAME = process.env.DB_NAME;
 const DB_USER = process.env.DB_USER;
 const DB_PASSWORD = process.env.DB_PASSWORD;
-// Connect to the database using parameters
+
+// SSL configuration with a single certificate
 const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
     host: DB_HOST,
     dialect: "mysql",
     port: DB_PORT,
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false,
+            ca: fs.readFileSync(path.resolve(__dirname, 'isrgrootx1.pem')).toString(),
+        }
+    }
 });
-// Establish connection with mysql database
+
+// Establish connection with MySQL database
 sequelize.authenticate()
     .then(() => {
         console.log('Connection established successfully!');
